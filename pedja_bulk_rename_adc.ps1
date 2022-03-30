efb69b051a68880dae1ea352029249a60551b411
pedjaj <pedjajks@gmail.com>
2022-03-30 11:51:19 +0200

    Add files via upload
    windowsa  7,8,10,11 i servera 2008>
    Powershell  v3.0 
    
    1.	Open PowerShell with "Run As Administrator“
    2.	adc.csv  - delimiter must be „ , “
    
    oldname,newname
    WEDSKS0022,RKVKS0110
    WEDSKS0117,RKVKS1413
    
    3.	.\pedja_bulk_rename_adc.ps1
    4.	log  nisuprosli 30-03-2022-1003.csv with comuters with errors
    
    
    Powershell skripta preimenovaće računare po adc.csv-u, proveri prvo da li je PC online ili ne, pa proveri da li novo ime možda postoji na mreži, pa tek onda vrši rename računara uz restart.
    
    Problem: na nekoliko pc sa windows 7 je izvršen rename u domenu, ali na pc je ostalo staro ime (ima neke veze sa vremenom). Ovo se rešava tako što se taj pc isčlani iz domena, promeni mu se ime manuelno i opet učlani u domen.
    Na velikoj većini win7 pc i na svim win10 pc nije bilo problema.

---
+++ pedja_bulk_rename_adc.ps1
@@ -0,0 +1,37 @@
+<#
+Bulk rename computers from AD;
+Predrag Jordanovic
+predrag.jordanovic@ods.rs
+29.03.2022
+v2
+
+Powershel >v3.0
+Open PowerShell using "Run As Administrator"
+adc.csv - delimiter mora biti ,
+u csv-u : oldname,newname
+
+.\bulk_rename_adc.ps1
+
+#>
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass -Force;
+#$computers = import-csv -Path "C:\cs\bulk_rename_adc\adc.csv"
+$computers = import-csv -Path ".\adc.csv"
+$Credential = Get-Credential
+$nisuprosli=".\nisuprosli $(get-date -f dd-MM-yyyy-HHMM).csv"
+$makecsv="oldname,newname" | Out-File $nisuprosli -Encoding utf8 -Append
+
+foreach ($pc in $computers){
+   
+ $IsOldPCalive=Test-Connection -ComputerName $pc.oldname -Quiet -Count 1 -ErrorAction SilentlyContinue
+ $IsNewPCalive=Test-Connection -ComputerName $pc.newname -Quiet -Count 1 -ErrorAction SilentlyContinue
+
+ if ($IsOldPCalive -eq $True -and $IsNewPCalive -eq $False) {
+      write-host "Rename PC $($pc.oldname) u $($pc.newname)" -ForegroundColor Cyan
+      Rename-computer -computername $pc.oldname -newname $pc.newname -domaincredential $Credential -PassThru -force -restart #-WhatIf   
+         }
+    else {
+    write-host "PC $($pc.oldname) nije dostupan ili vec postoji $($pc.newname)" -ForegroundColor Yellow
+    $makecsv="$($pc.oldname),$($pc.newname)" | Out-File $nisuprosli -Encoding utf8 -Append
+    }
+
+}
